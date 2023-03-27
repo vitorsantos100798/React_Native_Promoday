c1f483299363310b58ec9ddab0c6473225b7a94e
Vitor Santos <githubvitor@gmail.com>
2023-03-27 11:15:46 -0300

    create header

--- src/screens/Feed/index.tsx
+++ src/screens/Feed/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import {NavigateScreenProps} from '../../types/NavigateScreenProps.ts';
-import {Text} from 'react-native';
+import {Text, View, Image, TouchableOpacity} from 'react-native';
+import {styles} from './styles';
 
 function Feed({navigation}: NavigateScreenProps) {
+  const navigateProfile = () => {
+    navigation.navigate('profile');
+  };
+  const image = require('../../assets/profileFeed.png');
+  const cart = require('../../assets/cart.png');
+  const triangle = require('../../assets/triangle.png');
   return (
     <>
-      <Text>Tela de Feed</Text>
+      <View style={styles.container}>
+        <View style={styles.containerFlex}>
+          <View style={styles.logoContainer}>
+            <TouchableOpacity onPress={navigateProfile}>
+              <Image source={image} style={{marginRight: 10, marginTop: 5}} />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Image source={cart} />
+
+            <Text style={styles.title}>Presidente Prudente</Text>
+            <Image source={triangle} />
+          </View>
+        </View>
+      </View>
     </>
   );
 }
---
+++ src/screens/Feed/styles.tsx
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#DB3026',
+    width: '100%',
+    height: '13%',
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  logoContainer: {
+    width: '100%',
+    height: '35%',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  containerFlex: {
+    flex: 1,
+  },
+  title: {
+    fontFamily: 'Poppins-Medium',
+    color: 'white',
+    fontSize: 20,
+    marginRight: 15,
+    marginLeft: 15,
+  },
+});
