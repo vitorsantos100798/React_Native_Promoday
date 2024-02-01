93d0cada80f64eb9ed1da8859a8cfb1164c1f9b0
Vitor Santos <githubvitor@gmail.com>
2024-02-01 06:23:11 -0300

    add server on production for test at file api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, {AxiosRequestConfig} from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://10.0.2.2:8080',
+  baseURL: 'http://52.86.148.65:8080',
 });
 
 api.interceptors.request.use(
