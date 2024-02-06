1d852949af92e5247504ed778a073540211ee3ed
Vitor Santos <githubvitor@gmail.com>
2024-02-05 23:17:28 -0300

    update endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, {AxiosRequestConfig} from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://ec2-100-25-142-212.compute-1.amazonaws.com:8080',
+  baseURL: 'http://ec2-18-234-192-224.compute-1.amazonaws.com:8080',
 });
 
 api.interceptors.request.use(
